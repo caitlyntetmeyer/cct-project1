996e0f188dbbc2bf63dde703eb88fb1960937a97
Caitlyn Tetmeyer <caitlyntetmeyer@gmail.com>
2017-07-26 17:36:29 -0400

    deployment

--- db_mongo.js
+++ db_mongo.js
@@ -1,7 +1,7 @@
 module.exports = {
-	// databaseUrl: 'mongodb://caitlyntetmeyer1:MerryMen182@ds147551.mlab.com:47551/project1',
-	databaseUrl: 'mongodb://ds147551.mlab.com:47551/project1',
+	databaseUrl: 'mongodb://caitlyntetmeyer1:MerryMen182@ds147551.mlab.com:47551/project1',
+	// databaseUrl: 'mongodb://ds147551.mlab.com:47551/project1',
 	// databaseUrl: 'mongodb://caitlyntetmeyer1:MerryMen182@ds147882.mlab.com:47551/project1',
-	// secret: 'MerryMen182'
-	secret: 'yesterdaysadairflying'
+	secret: 'MerryMen182'
+	// secret: 'yesterdaysadairflying'
 }
